f5f9ea1117fc4f45658548c2540292b240fa9e36
MekImEne <fi_mekideche@esi.dz>
2020-04-04 21:29:40 +0200

    components

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,45 @@
 <template>
   <div>
-    <h1>{{title}}</h1>
-    <!--<p>{{greeting()}}</p>-->
-    <ninjas></ninjas>
+    <!--<h1>{{title}}</h1>
+    <p>{{greeting()}}</p>
+    <ninjas></ninjas>-->
+    <app-header></app-header>
+    <app-ninjas></app-ninjas>
+    <app-footer></app-footer>
   </div>
 </template>
 
 <script>
 /* Nesting a component locally */
-import Ninjas from "./Ninjas.vue";
+/*import Ninjas from "./Ninjas.vue";*/
+import Header from "./components/Header.vue";
+import Footer from "./components/Footer.vue";
+import Ninjas from "./components/Ninjas";
 
 export default {
   components: {
-    ninjas: Ninjas
+    /*ninjas: Ninjas*/
+    "app-header": Header,
+    "app-footer": Footer,
+    "app-ninjas": Ninjas
   },
   data() {
     return {
-      /*title: "Our first vue page ! Aoh"*/
-      title: "IMENE app"
+      /*title: "Our first vue page ! Aoh"
+      title: "IMENE app"*/
     };
-  },
-  methods: {
-    /*greeting: function() {
-      return "Hello Mimi :D !";
-    }*/
   }
+  /*methods: {
+    greeting: function() {
+      return "Hello Mimi :D !";
+    }
+  }*/
 };
 </script>
-
+/*
 <style scoped>
 h1 {
   color: purple;
 }
 </style>
+*/
---
+++ src/components/Ninjas.vue
@@ -0,0 +1,81 @@
+<template>
+  <div id="ninjas">
+    <h1>List of Ninjas :</h1>
+    <ul>
+      <li v-for="ninja in ninjas" v-bind:key="ninja" v-on:click="ninja.show = !ninja.show">
+        <h2>{{ninja.name}}</h2>
+        <h3 v-show="ninja.show">{{ninja.speciality}}</h3>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      /* ninjas: ["Imene", "Abdennour", "Amina", "Aya"]*/
+
+      ninjas: [
+        {
+          name: "MEKIDECHE Imene",
+          speciality: "Vue components",
+          show: false
+        },
+        {
+          name: "AMOKRANE Abdennour",
+          speciality: "HTML Wizardry",
+          show: false
+        },
+        {
+          name: "MEHERHERA Amina",
+          speciality: "Click Events",
+          show: false
+        },
+        {
+          name: "MEK Aya",
+          speciality: "Conditionals",
+          show: false
+        },
+        {
+          name: "MEK Houda",
+          speciality: "Webpack",
+          show: false
+        },
+        {
+          name: "MEK Ikram",
+          speciality: "Data Diggin",
+          show: false
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style scoped>
+#ninjas {
+  width: 100%;
+  max-width: 1200px;
+  margin: 40px auto;
+  padding: 0 20px;
+  box-sizing: border-box;
+}
+
+ul {
+  display: felx;
+  flex-wrap: wrap;
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  flex-grow: 1;
+  flex-basis: 300px;
+  text-align: center;
+  padding: 30px;
+  border: 1px solid #222;
+  margin: 10px;
+}
+</style>
+
