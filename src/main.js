b4099761f45dfdea9568977ac0d095dbae748e36
MekImEne <fi_mekideche@esi.dz>
2020-04-05 22:13:47 +0200

    add Blog aapplication

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+//import App from './App.vue'
+import Blog from "./Blog.vue";
+import VueResource from "vue-resource";
+
 /* Nesting a component globally
 import Ninjas from './Ninjas.vue'
 Vue.component('ninjas', Ninjas)
@@ -8,8 +11,15 @@ Vue.component('ninjas', Ninjas)
 /*Event Bus */
 export const bus = new Vue();
 
-
+/*
 new Vue({
     el: '#app',
     render: h => h(App)
 })
+
+*/
+new Vue({
+    el: "#blog",
+    render: h => h(Blog)
+});
+Vue.use(VueResource);
